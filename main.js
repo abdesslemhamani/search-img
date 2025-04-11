f328169ac3baebb8d52ba6d08781d31def2b3c76
abdesslemhamani <abdesslemhamani@gmail.com>
2025-04-11 16:54:24 +0100

    final commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const form = document.getElementById("search-form");
+const input = document.getElementById("search-input");
+const results = document.getElementById("search-results");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const query = input.value;
+    results.innerHTML = "Searching...";
+
+    const accessKey = "9FSLHlxhEsJlUJbqqNyHpmoHQDel3qkQRDPOVQBdjIA";
+
+    const response = await fetch(
+        `https://api.unsplash.com/search/photos?query=${query}&per_page=9&client_id=${accessKey}`
+    );
+
+    const data = await response.json();
+
+    results.innerHTML = "";
+    data.results.forEach((photo) => {
+        const img = document.createElement("img");
+        img.src = photo.urls.small;
+        results.appendChild(img);
+    });
+});
